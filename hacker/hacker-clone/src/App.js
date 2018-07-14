627b88ec41bc2e147654cf790be1986feefaf920
Haley Sulcer <hjsulcer@gmail.com>
2018-07-13 22:16:02 -0400

    added hot module replacement to index.js

--- hacker/hacker-clone/src/App.js
+++ hacker/hacker-clone/src/App.js
@@ -1,18 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
+    const helloWorld = 'Howdy neighbor!';
+    const info = 'Hopefully I will complete an app at the end of this book.';
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h2>{helloWorld}</h2>
+        <h3>{info}</h3>
       </div>
     );
   }
