11add0bb73ef742c60b430ce089e9613772b4498
Haley Sulcer <hjsulcer@gmail.com>
2018-07-13 22:46:21 -0400

    condensed App.js

--- hacker/hacker-clone/src/App.js
+++ hacker/hacker-clone/src/App.js
@@ -1,14 +1,40 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const list = [
+  {
+    title: 'React',
+    url: 'https://facebook.github.io/react/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://github.com/reactjs/redux',
+    author: 'Abramov, Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
 class App extends Component {
   render() {
-    const helloWorld = 'Howdy neighbor!';
-    const info = 'Hopefully I will complete an app at the end of this book.';
     return (
       <div className="App">
-        <h2>{helloWorld}</h2>
-        <h3>{info}</h3>
+        {list.map(item => 
+            <div>
+              <span>
+                <a href={item.url}>{item.title}</a>
+              </span>
+              <span>{item.author} </span>
+              <span>{item.num_comments} </span>
+              <span>{item.points} </span>
+            </div>
+          )
+        }
       </div>
     );
   }
